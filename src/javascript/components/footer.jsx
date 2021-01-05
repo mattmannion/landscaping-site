ec6d6cb242c29cd22b051bba6eadeb12389741c8
Khris Rhodes <khrisrhds@gmail.com>
2021-01-05 14:37:17 -0700

    worked on footer

--- src/javascript/components/footer.jsx
+++ src/javascript/components/footer.jsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import React from "react";
 
 export default function footer() {
-  return (
-    <>
-      <footer className='footer__container'>footer</footer>
-    </>
-  );
+    return (
+        <>
+            <footer className="footer">
+                <div className="footer__container">
+                    <div className="footer__logo">logo</div>
+                    <div className="footer__links">
+                        <button className="footer__Nav">services</button>
+                        <button className="footer__Nav">about</button>
+                        <button className="footer__Nav">contact</button>
+                    </div>
+                </div>
+            </footer>
+        </>
+    );
 }
