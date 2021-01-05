b7e2db7af8c8dfb7eeb4afbdf9f3ebb4bfe51986
Matt Mannion <mattpmannion@gmail.com>
2021-01-05 15:49:35 -0500

    jsx component refactor

--- src/javascript/app.jsx
+++ src/javascript/app.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import Nav from './components/nav';
 import Header from './components/header';
@@ -16,23 +15,4 @@ export default function App() {
       </div>
     </>
   );
-=======
-import React from "react";
-import Nav from "./components/nav";
-import Header from "./components/header";
-import Main from "./components/main";
-import Footer from "./components/footer";
-
-export default function App() {
-    return (
-        <>
-            <div className="body-container">
-                <Nav />
-                <Header />
-                <Main />
-                <Footer />
-            </div>
-        </>
-    );
->>>>>>> 7ab4fb55349ef1df4bbf9fc2fa94af74167794dd
 }
