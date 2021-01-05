767cdcf4531ada3bc2cceebdb902f7247137e434
Khris Rhodes <khrisrhds@gmail.com>
2021-01-05 14:54:57 -0700

    testing

--- src/javascript/components/header.jsx
+++ src/javascript/components/header.jsx
@@ -1,40 +1,23 @@
-import React from 'react';
-import Hero from '../../resources/img/excavator-main.PNG';
+import React from "react";
+import Hero from "../../resources/img/excavator-main.PNG";
 
 export default function header() {
-<<<<<<< HEAD
-    return (
-        <>
-            <header className="header">
-                <div className="company">
-                    <h1 className="company-title">khris's word wrap tester</h1>
-                </div>
-
-                <button className="nav-snap">
-                    <div>See more</div>
-                    <div>&#8595;</div>
-                </button>
-
-                <img className="hero-image" src={Hero} alt="" />
-            </header>
-        </>
-    );
-=======
   return (
     <>
-      <header className='header'>
-        <div className='company'>
-          <h1 className='company-title'>khris's word wrap tester</h1>
+      <header className="header">
+        <div className="company">
+          <h1 className="company-title">khris's word wrap tester</h1>
         </div>
-        <button className='nav-snap'>
+
+        <button className="nav-snap">
           <div>See more</div>
           <div>&#8595;</div>
         </button>
-        <img className='hero-image' src={Hero} alt='' />
+
+        <img className="hero-image" src={Hero} alt="" />
       </header>
     </>
   );
->>>>>>> 122b33bb8fd1dfbe930207708ab1b2e13eaec703
 }
 
 //
