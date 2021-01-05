6df16b20072ff949ca8486c5c1a5c88133090f39
Matt Mannion <mattpmannion@gmail.com>
2021-01-05 12:52:01 -0500

    fonts and scss added

--- src/javascript/app.jsx
+++ src/javascript/app.jsx
@@ -3,7 +3,25 @@ import React from 'react';
 export default function App() {
   return (
     <>
-      <div>test</div>
+      <div className='body-container'>
+        <nav className='navigation'>
+          <div className='navigation__container'>
+            <div className='navigation__logo'>logo</div>
+            <div className='navigation__links'>
+              <div>services</div>
+              <div>about</div>
+              <div>contact</div>
+            </div>
+          </div>
+        </nav>
+        <header className='header'>header</header>
+        <main className='main'>
+          <div>section 1</div>
+          <div>section 2</div>
+          <div>section 3</div>
+        </main>
+        <footer className='footer__container'>footer</footer>
+      </div>
     </>
   );
 }
