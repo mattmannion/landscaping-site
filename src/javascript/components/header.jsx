122b33bb8fd1dfbe930207708ab1b2e13eaec703
Matt Mannion <mattpmannion@gmail.com>
2021-01-05 16:33:31 -0500

    more styling adding to navigation

--- src/javascript/components/header.jsx
+++ src/javascript/components/header.jsx
@@ -1,21 +1,21 @@
-import React from "react";
-import Hero from "../../resources/img/excavator-main.PNG";
+import React from 'react';
+import Hero from '../../resources/img/excavator-main.PNG';
 
 export default function header() {
-    return (
-        <>
-            <header className="header">
-                <div className="company">
-                    <h1 className="company-title">khris's word wrap tester</h1>
-                </div>
-                <button className="nav-snap">
-                    <div>See more</div>
-                    <div>&#8595;</div>
-                </button>
-                <img className="hero-image" src={Hero} alt="" />
-            </header>
-        </>
-    );
+  return (
+    <>
+      <header className='header'>
+        <div className='company'>
+          <h1 className='company-title'>khris's word wrap tester</h1>
+        </div>
+        <button className='nav-snap'>
+          <div>See more</div>
+          <div>&#8595;</div>
+        </button>
+        <img className='hero-image' src={Hero} alt='' />
+      </header>
+    </>
+  );
 }
 
 //<button className="snap-button"></button>
