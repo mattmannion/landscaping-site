d671bfa264556fc3f3f3da38f679617a1be2adb2
Matt Mannion <mattpmannion@gmail.com>
2021-01-05 11:01:55 -0500

    init

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './app';
+import App from './javascript/app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
