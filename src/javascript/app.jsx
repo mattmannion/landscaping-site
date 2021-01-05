7ab4fb55349ef1df4bbf9fc2fa94af74167794dd
Khris Rhodes <khrisrhds@gmail.com>
2021-01-05 13:46:57 -0700

    adjusted header

--- src/javascript/app.jsx
+++ src/javascript/app.jsx
@@ -1,27 +1,18 @@
-import React from 'react';
+import React from "react";
+import Nav from "./components/nav";
+import Header from "./components/header";
+import Main from "./components/main";
+import Footer from "./components/footer";
 
 export default function App() {
-  return (
-    <>
-      <div className='body-container'>
-        <nav className='navigation'>
-          <div className='navigation__container'>
-            <div className='navigation__logo'>logo</div>
-            <div className='navigation__links'>
-              <div>services</div>
-              <div>about</div>
-              <div>contact</div>
+    return (
+        <>
+            <div className="body-container">
+                <Nav />
+                <Header />
+                <Main />
+                <Footer />
             </div>
-          </div>
-        </nav>
-        <header className='header'>header</header>
-        <main className='main'>
-          <div>section 1</div>
-          <div>section 2</div>
-          <div>section 3</div>
-        </main>
-        <footer className='footer__container'>footer</footer>
-      </div>
-    </>
-  );
+        </>
+    );
 }
--- src/javascript/components/header.jsx
+++ src/javascript/components/header.jsx
@@ -1,9 +1,21 @@
-import React from 'react';
+import React from "react";
+import Hero from "../../resources/img/excavator-main.PNG";
 
 export default function header() {
-  return (
-    <>
-      <header className='header'>header</header>
-    </>
-  );
+    return (
+        <>
+            <header className="header">
+                <div className="company">
+                    <h1 className="company-title">khris's word wrap tester</h1>
+                </div>
+                <button className="nav-snap">
+                    <div>See more</div>
+                    <div>&#8595;</div>
+                </button>
+                <img className="hero-image" src={Hero} alt="" />
+            </header>
+        </>
+    );
 }
+
+//<button className="snap-button"></button>
